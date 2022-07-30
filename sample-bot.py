631e07ea80a7dedb664273767247a13511b9b81c
phychee <102762019+phychee@users.noreply.github.com>
2022-07-30 12:12:19 -0700

    Update sample-bot.py

--- sample-bot.py
+++ sample-bot.py
@@ -43,7 +43,7 @@ def main():
     # unlikely it will be traded against. Maybe there is a better price to
     # pick? Also, you will need to send more orders over time.
     exchange.send_add_message(order_id=1, symbol="BOND", dir=Dir.BUY, price=999, size=1)
-    exchange.send_add_message(order_id=1, symbol="BOND", dir=Dir.BUY, price=1001, size=1)
+    exchange.send_add_message(order_id=1, symbol="BOND", dir=Dir.SELL, price=1001, size=1)
 
     # Set up some variables to track the bid and ask price of a symbol. Right
     # now this doesn't track much information, but it's enough to get a sense
